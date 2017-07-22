ae3015e840e0d95da7f7f696c05986aea594940e
uliang <uliang6482@gmail.com>
2017-07-23 01:09:43 +0800

    started on not perfect power. Running into overflow errors

--- AKS.py
+++ AKS.py
@@ -6,7 +6,8 @@ The algorithm outline:
 
 Input n > 1
 
-1. Check that n is not a perfect power. If it is output composite
+1. Check that n is not a perfect power a^b for some a>1 and b>1.
+   If it is output composite
 
 2. Find smallest r such that ord_r(n) > (log_2 n)^2. Skip this r if 
    gcd(r,n) != 1
@@ -31,9 +32,29 @@ Email: tang_u_liang@sp.edu.sg
 _________________________________________________________________________
 """
 
+import math
+
+
 def gcd(m,n):
     while m%n != 0:
         m, n = n, m%n
 
     return n
+
+def not_perfect_power(n):
+    """
+    Checks that n is not a perfect power i.e. n=a^b for a>1 and b>1
+    
+    Returns
+        bool, True indicates that it is not a perfect power. Therefore,
+              False indicates that n is composite
+    """
+    M = math.floor(math.log(n, 2))+1
+    
+    for b in range(2, M):
+        a = pow(n, 1/b)
+        
+        if math.floor(a)-a == 0:
+            return False
+    return True
 
